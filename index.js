23e095140be05ab6db06298dfc2c635cf685aa1c
wayungi <fwayungi@gmail.com>
2026-05-28 15:54:21 +0300

    ADD: login end point

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ const user = {
     username:"wayncis",
     password: "pass@123"
 }
+
+app.post("/login", (req, res) => {
+    const {username, password} = req.body;
+
+    if(username !== user.username || password !== user.password) return res.status(401).json({message: "invalid credentials"})
+
+    const accessToken = jwt.sign({username, id: user.id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "5m"})
+    const refreshToken = jwt.sign({username, id: user.id}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: "3d"})
+
+    res.json({accessToken, refreshToken})
+
+})
