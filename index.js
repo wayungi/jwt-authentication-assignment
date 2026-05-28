843e989a6bd387ea95a502aebbb9f825e2a0006f
wayungi <fwayungi@gmail.com>
2026-05-28 15:39:51 +0300

    ADD: test user

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const app = express();
 
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+
+//TEST USER
+const user = {
+    id:1,
+    username:"wayncis",
+    password: "pass@123"
+}
